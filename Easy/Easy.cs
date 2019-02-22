1adceecd564227f2ba4a32317825b8079f5c6efb
Lyubomir Georgiev <LuGeorgiev@globul.bg>
2019-02-22 08:39:16 +0200

    first bunch of solved problems

---
+++ Easy/Easy.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Linq;
+
+namespace Easy
+{
+    class Easy
+    {
+        static void Main(string[] args)
+        {
+            //9. Palindrome Number
+            //Console.WriteLine(IsPalindrome(int.Parse(Console.ReadLine())));
+
+            //14 Longest Common Prefix
+            //Console.WriteLine(LongestCommonPrefix(new[] { "dog","racecar","car" }));
+
+            //35. Search Insert Position
+            //Console.WriteLine(SearchInsert(new[] { 1, 3, 5, 6 }, 0));
+
+            //27. Remove Element
+            //Console.WriteLine(RemoveElement(new[] { 3, 2, 2, 3 },2));
+
+            //198. House Robber
+            Console.WriteLine(Rob(new[] {2, 7, 9, 3, 1}));
+        }
+        //9. Palindrome Number
+        public static bool IsPalindrome(int x)
+        {
+            var str = x.ToString();
+            if (x<0)
+            {
+                return false;
+            }
+            if(str.Length==1 )
+            {
+                return true;
+            }
+            for (int i = 0; i < str.Length/2; i++)
+            {
+                if (str[i]!=str[str.Length-1-i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        //14 Longest Common Prefix
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            var result = "";
+            if (strs.Length==0)
+            {
+                return result;
+            }
+            if (strs.Length==1)
+            {
+                return strs[0];
+            }
+            try
+            {
+                for (int i = 0; i < strs[0].Length; i++)
+                {
+                    for (int k = 1; k < strs.Length; k++)
+                    {
+                        if (strs[0][i]!=strs[k][i])
+                        {
+                            return result;
+                        }
+                    }
+                    result += strs[0][i];
+                }
+            }
+            catch (Exception)
+            {
+                return result;
+            }
+
+            return result;
+        }
+
+        //35. Search Insert Position
+        public static int SearchInsert(int[] nums, int target)
+        {
+            int index = 0;
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            else if (nums.Length == 1)
+            {
+                if (nums[0] >= target)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+
+            index = BinarySearch(nums, target, 0, nums.Length - 1);
+
+            return index;
+        }
+        private static int BinarySearch(int[] nums, int target, int inIndex, int outIndex)
+        {
+            if (outIndex-inIndex<=1 )
+            {
+                if (nums[inIndex] == target)
+                {
+                    return inIndex;
+                }
+                else if (nums[outIndex]==target)
+                {
+                    return outIndex;
+                }
+                else if (nums[outIndex] <= target)
+                {
+                    return outIndex+1;
+                }
+                else if (nums[inIndex] > target)
+                {
+                    return inIndex ;
+                }
+                else
+                {
+                    return outIndex;
+                }
+
+            }
+
+            int centerIndex = (outIndex + inIndex) / 2;
+            int index = 0;
+            if (nums[centerIndex]==target)
+            {
+                return (centerIndex);
+            }
+            if (nums[centerIndex]<target)
+            {
+               index= BinarySearch(nums, target, centerIndex, outIndex);
+            }
+            else
+            {
+                index=BinarySearch(nums, target, inIndex, centerIndex);
+            }
+
+            return index;
+        }
+
+        //27. Remove Element
+        public static int RemoveElement(int[] nums, int val)
+        {
+            return nums.Where(x => x != val).ToArray().Count();
+        }
+
+        //198. House Robber
+        public static int Rob(int[] nums)
+        {
+            if (nums.Length==1)
+            {
+                return nums[0];
+            }
+            var length = nums.Length;
+            var result = new int[length];
+            result[0] = nums[0];
+            result[1] = Math.Max(nums[0], nums[1]);
+            for (int i = 2; i < length; i++)
+            {
+                result[i] = Math.Max(result[i - 1], nums[i] + result[i - 2]);
+            }
+
+            return result.OrderByDescending(x => x).ToArray()[0];
+        }
+    }
+}
---
+++ Medium/Meduim.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Medium
+{
+    class Meduim
+    {
+        static void Main(string[] args)
+        {
+            //165.Compare Version Numbers
+            //Console.WriteLine(CompareVersion("1.0", "1.01"));
+
+            //153.Find Minimum in Rotated Sorted Array
+            Console.WriteLine(FindMin(new[] { 3,2,1 }));
+        }
+
+        //165.Compare Version Numbers
+        public static int CompareVersion(string version1, string version2)
+        {
+
+            List<int> firstStr = ConvToIntArray(version1);
+            List<int> secondStr = ConvToIntArray(version2);
+            int longerLen = firstStr.Count >= secondStr.Count
+                    ? firstStr.Count
+                    : secondStr.Count;
+
+            firstStr = FillWithZeroes(firstStr, longerLen);
+            secondStr = FillWithZeroes(secondStr, longerLen);
+
+            for (int i = 0; i < longerLen; i++)
+            {
+                if (firstStr[i] < secondStr[i])
+                {
+                    return -1;
+                }
+                else if (firstStr[i] > secondStr[i])
+                {
+                    return 1;
+                }
+            }        
+
+            return 0;
+        }
+        private static List<int> FillWithZeroes(List<int> nums, int length)
+        {
+            for (int i = nums.Count-1; i < length; i++)
+            {
+                nums.Add(0);
+            }
+            return nums;
+        }
+        private static List<int> ConvToIntArray(string version1)
+        {
+            var result = version1
+                    .Split('.', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+            return result;
+        }
+
+        //153.Find Minimum in Rotated Sorted Array
+        public static int FindMin(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            else if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+            else if (nums.Length == 2)
+            {
+                if (nums[0] < nums[1])
+                {
+                    return nums[0];
+                }
+                else
+                {
+                    return nums[1];
+                }
+            }
+            else if (nums[0] < nums[1] && nums[0]<nums[nums.Length-1])
+            {
+                return nums[0];
+            }
+            else if (nums[nums.Length-2]>nums[nums.Length-1])
+            {
+                return nums[nums.Length - 1];
+            }
+
+            int minIndex = BinaryRotetedSearch(nums, 0, nums.Length-1);
+            return nums[minIndex];
+        }
+        private static int BinaryRotetedSearch(int[] nums, int lowIndex, int highIndex)
+        {
+            int centralIndex = (highIndex + lowIndex) / 2;
+
+            var lowValue = nums[centralIndex - 1];
+            var central = nums[centralIndex];
+            var hiValue = nums[centralIndex + 1];
+
+            if (nums[centralIndex-1]>nums[centralIndex] && nums[centralIndex] < nums[centralIndex+1])
+            {
+                return centralIndex;
+            }
+
+            if (nums[centralIndex]> nums[lowIndex] )
+            {
+                centralIndex = BinaryRotetedSearch(nums, centralIndex, highIndex);
+            }
+            else
+            {
+                centralIndex = BinaryRotetedSearch(nums, lowIndex, centralIndex);
+
+            }
+
+            return centralIndex;
+        }
+    }
+}
