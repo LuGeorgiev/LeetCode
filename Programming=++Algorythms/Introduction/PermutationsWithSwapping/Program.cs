b8a25b84f58217a3384d59c5f8a3afef384e397a
LuGeorgiev <lubomir_78@abv.bg>
2020-03-03 20:08:33 +0200

    permutations

---
+++ Programming=++Algorythms/Introduction/PermutationsWithSwapping/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PermutationsWithSwapping
+{
+    class Program
+    {
+        private const int elements = 3;
+        private static int[] vector = new int[elements];
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < elements; i++)
+            {
+                vector[i] = i;
+            }
+
+            Permute(elements);
+        }
+
+        private static void Print()
+        {
+            for (int i = 0; i < elements; i++)
+            {
+                Console.Write($"{vector[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void Permute(int index)
+        {
+            if (index == 0)
+            {
+                Print();
+            }
+            else
+            {
+                Permute(index - 1);
+
+                for (int k = 0; k < index -1; k++)
+                {
+                    int swap = vector[k];
+                    vector[k] = vector[index - 1];
+                    vector[index - 1] = swap;
+
+                    Permute(index - 1);
+
+                    swap = vector[k];
+                    vector[k] = vector[index - 1];
+                    vector[index - 1] = swap;
+                }
+            }           
+        }
+    }
+}
---
+++ Programming=++Algorythms/Introduction/Prmutations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Prmutations
+{
+    class Program
+    {
+        private const int elements = 4;
+        private static bool[] used = new bool[elements];
+        private static int[] vector = new int[elements];
+
+        static void Main(string[] args)
+        {   
+            Permute(0);
+        }
+
+        private static void Print()
+        {
+            for (int i = 0; i < elements; i++)
+            {
+                Console.Write($"{vector[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= elements)
+            {
+                Print();
+                return;
+            }
+
+            for (int k = 0; k < elements; k++)
+            {
+                if (! used[k])
+                {
+                    used[k] = true;
+                    vector[index] = k;
+                    Permute(index + 1);
+                    used[k] = false;
+                }
+            }
+        }
+    }
+}
