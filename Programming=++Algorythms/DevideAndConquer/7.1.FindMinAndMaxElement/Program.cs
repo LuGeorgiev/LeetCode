663486daf60499050751a927914c5d8287a4d336
LuGeorgiev <lubomir_78@abv.bg>
2023-04-11 14:37:53 +0300

    kth element

--- Programming=++Algorythms/DevideAndConquer/7.1.FindMinAndMaxElement/Program.cs
+++ Programming=++Algorythms/DevideAndConquer/7.1.FindMinAndMaxElement/Program.cs
@@ -4,7 +4,76 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var arr = new int[] { 1, 2, 3 ,-5, 10, -89, 8, 112};
+
+            //Console.WriteLine($"Max element is: {FindMax(arr)}");
+
+            //var minMax = FindMinMax(arr);
+            //Console.WriteLine($"Max element is: {minMax.max} and min is: {minMax.min}");
+
+            Console.WriteLine($"Second biggest is: {SeconBiggest(arr)}");
+        }
+
+        private static int FindMax(int[] input)
+        {
+            var max = input[0];
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] > max)
+                    max = input[i];
+            }
+            return max;
+        }
+
+        private static (int min, int max) FindMinMax(int[] input)
+        {
+            var len = input.Length;
+            var middle = len / 2;
+            var minVal = input[middle];
+            var maxVal = input[middle];
+
+            for (int i = 0; i < middle; i++)
+            {
+                if (input[i] > input[len - i - 1])
+                {
+                    if (input[i] > maxVal)
+                        maxVal = input[i];
+                    if (input[len - i - 1] < minVal)
+                        minVal = input[len - i - 1];
+                }
+                else
+                {
+                    if (input[i] < minVal)
+                        minVal = input[i];
+                    if (input[len - i - 1] > maxVal)
+                        maxVal = input[len - i - 1];
+                }
+            }
+
+            return (minVal, maxVal);
+        }
+
+        private static int SeconBiggest(int[] input)
+        {
+            int biggest = input[0];
+            int seconBiggest = input[1];
+            if (biggest < seconBiggest)
+                (seconBiggest, biggest) = (biggest, seconBiggest);
+
+            for (int i = 2; i < input.Length; i++)
+            {
+                if (input[i] > seconBiggest)
+                {
+                    seconBiggest = input[i];
+                    if (seconBiggest > biggest)
+                    {
+                        (biggest, seconBiggest) = (seconBiggest, biggest);
+                    }
+                }
+            }
+
+            return seconBiggest;
         }
     }
 }
---
+++ Programming=++Algorythms/DevideAndConquer/7.1.KthBiggestElement/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Linq;
+
+namespace _7._1.KthBiggestElement
+{
+    internal class Program
+    {
+        private const int ARRAY_ELMENTS = 4; //n
+        private const int SEARCHED_ELEMENT = 3; //k
+        private static int[] arr = new int[ARRAY_ELMENTS]; //m
+
+        static void Main(string[] args)
+        {
+            var min = -100;
+            var max = 100;
+            var rnd = new Random();
+            arr = Enumerable.Repeat(0, ARRAY_ELMENTS)
+                .Select(x => rnd.Next(min, max))
+                .ToArray();
+
+            Console.WriteLine($"Array before searching:\n{string.Join(", ", arr)}");
+            Console.WriteLine($"Looking for {SEARCHED_ELEMENT}th element.");
+
+            HeapFindKth(SEARCHED_ELEMENT);
+
+            Console.WriteLine($"{SEARCHED_ELEMENT}th element is: {arr[SEARCHED_ELEMENT-1]}");
+            Console.WriteLine($"Array after search is; {string.Join(", " , arr)}");
+
+        }
+
+        /* Отсява елем. от върха на пирамидата */
+        private static void ShiftMin(int left, int right)
+        {
+            var i = left;
+            var j = i + i +1;
+            var x = arr[i];
+
+            while (j <= right)
+            {
+                if (j < right)
+                    if (arr[j] > arr[j + 1])
+                        j++;
+
+                if (x <= arr[j])
+                    break;
+
+                arr[i] = arr[j];
+                i = j;
+                j = j * 2 + 1;
+            }
+
+            arr[i] = x; 
+        }
+
+        /* Отсява елем. от върха на пирамидата */
+        private static void ShiftMax(int left, int right)
+        {
+            var i = left;
+            var j = i + i + 1;
+            var x = arr[i];
+
+            while (j <= right)
+            {
+                if (j < right)
+                    if (arr[j] < arr[j + 1])
+                        j++;
+
+                if (x >= arr[j])
+                    break;
+
+                arr[i] = arr[j];
+                i = j;
+                j = j * 2 + 1;
+            }
+
+            arr[i] = x;
+        }
+
+        private static void HeapFindKth(int kthElement)
+        {
+            int left;
+            int right;
+            var useMax = kthElement > ARRAY_ELMENTS / 2;
+            if (useMax)
+                kthElement = ARRAY_ELMENTS - kthElement - 1;
+            left = ARRAY_ELMENTS / 2;
+            right = ARRAY_ELMENTS - 1;
+
+            /* Построяване на пирамидата */
+            while (left -- > 0)
+                if(useMax)
+                    ShiftMax(left, right);
+                else
+                    ShiftMin(left, right);
+
+            /*(k - 1) - кратно премахване на минималния елемент */
+
+            for (int i = ARRAY_ELMENTS - 1; i >= ARRAY_ELMENTS - kthElement; i--)
+            {
+                arr[0] = arr[i];
+                if (useMax)
+                    ShiftMax(0, i);
+                else
+                    ShiftMin(0, i);
+            }
+        }
+    }
+}
