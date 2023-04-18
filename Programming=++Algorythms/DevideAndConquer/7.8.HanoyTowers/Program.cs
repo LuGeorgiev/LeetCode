1210638a535dc5736de7f28840485e5c9a7b53e7
LuGeorgiev <lubomir_78@abv.bg>
2023-04-18 17:19:09 +0300

    Hanoy towers

---
+++ Programming=++Algorythms/DevideAndConquer/7.8.HanoyTowers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace _7._8.HanoyTowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var disksCount = 5;
+            Console.WriteLine($"Number of disks: {disksCount}");
+            Hanoy('A', 'C', 'B', disksCount);
+        }
+
+        private static void DiscMoved(int n, char a, char b) =>        
+            Console.WriteLine($"Move disk {n} from {a} to {b}.");
+        
+
+        private static void Hanoy(char a, char c, char b, int numb)
+        {
+            if (numb == 1)
+            {
+                DiscMoved(1, a, c);
+            }
+            else 
+            {
+                Hanoy(a, b, c, numb - 1);
+                DiscMoved(numb, a, c);
+                Hanoy(b, c, a, numb - 1);
+            }
+        }
+    }
+}
