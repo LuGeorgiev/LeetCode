5ab6d48df4c1cfc817022f9a17db76564fa1ed3e
LuGeorgiev <lubomir_78@abv.bg>
2023-04-24 15:33:30 +0300

    shift left one additional C# scenario

---
+++ Programming=++Algorythms/DevideAndConquer/7.10.ShiftLeft_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _7._10.ShiftLeft_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = 12;
+            var shiftBy = 8;
+            int[] arr = InitArray(elements);
+            Console.WriteLine(string.Join(" ", arr));
+            ShiftLeft(ref arr, shiftBy);
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void ShiftLeft(ref int[] arr, int shiftBy)
+        {
+            // Кърниган и Плоджер
+
+            Reverse(ref arr, 0, shiftBy - 1);
+            Reverse(ref arr, shiftBy, arr.Length - 1);
+            Reverse(ref arr, 0, arr.Length - 1);
+        }
+
+        private static void Reverse(ref int[] arr, int start, int end)
+        {
+            var count = (end - start) / 2;
+            for (int i = 0; i <= count; i++, start++, end--)            
+                (arr[start], arr[end]) = (arr[end], arr[start]);
+            
+        }
+
+        private static int[] InitArray(int elements)
+        {
+            var result = new int[elements];
+            for (int i = 0; i < elements; i++)
+                result[i] = i;
+
+            return result;
+        }
+    }
+}
---
+++ Programming=++Algorythms/DevideAndConquer/7.10.ShiftLeft_3_CSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace _7._10.ShiftLeft_3_CSharp
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = 12;
+            var shiftBy = 8;
+            var arr = InitArray(elements);
+            Console.WriteLine(string.Join(" ", arr));
+            var result = ShiftLeft(arr, shiftBy);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static List<int> ShiftLeft(List<int> input, int shiftBy)
+        {
+            var first = input.GetRange(0, shiftBy);
+            var second = input.GetRange(shiftBy , input.Count - shiftBy );
+
+            first.Reverse();
+            second.Reverse();
+
+            var result = first.Concat(second).ToList();
+            result.Reverse();
+
+            return result;
+        }
+
+        private static List<int> InitArray(int elements)
+        {
+            var result = new List<int>(elements);
+
+            for (int i = 0; i < elements; i++)
+                result.Add(i);
+
+            return result;
+        }
+    }
+}
