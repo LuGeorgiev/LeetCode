7127e587f65eab32c082ff41d75f19da550ed603
LuGeorgiev <lubomir_78@abv.bg>
2023-04-25 10:37:44 +0300

    Dynamic programing init solution

---
+++ Programming=++Algorythms/DynamicProgramming/8.1.Knapsack_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _8._1.Knapsack_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
