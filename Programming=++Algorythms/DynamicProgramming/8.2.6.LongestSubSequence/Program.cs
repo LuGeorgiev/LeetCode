e489956a106411a14aa55f1e409177dbaf775d37
LuGeorgiev <lubomir_78@abv.bg>
2023-05-26 16:39:56 +0300

    longest subsequence length

---
+++ Programming=++Algorythms/DynamicProgramming/8.2.6.LongestSubSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _8._2._6.LongestSubSequence
+{
+    internal class Program
+    {
+        private const string FirstLine = "acbcacbcaba";
+        private const string SecondLine = "abacacacababa";
+
+        private static readonly int firstLength = FirstLine.Length; //m
+        private static readonly int secondLength = SecondLine.Length; //n
+        private static readonly int[,] matrix = new int[firstLength + 1, secondLength + 1];
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The length of longest common sub sequens is: {LongestCommonSubsetLength()}");
+        }
+
+        private static int LongestCommonSubsetLength()
+        {
+            for (int i = 1; i <= firstLength; i++)
+                for (int j = 1; j <= secondLength; j++)
+                    if (FirstLine[i - 1] == SecondLine[j - 1])
+                        matrix[i, j] = matrix[i - 1, j - 1] + 1;
+                    else
+                        matrix[i, j] = Math.Max(matrix[i - 1, j], matrix[i, j - 1]);
+
+            return matrix[firstLength, secondLength];
+        }
+
+        
+    }
+}
